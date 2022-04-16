8a41ce1f634b24926622af8b7535e19b4f0115c4
Kristaps Ritins <kritinsdev@gmail.com>
2022-04-16 18:56:51 +0300

    refactor script for validation

--- Form Validator/script.js
+++ Form Validator/script.js
@@ -16,40 +16,51 @@ function showSuccess(inputField) {
     formControl.classList.add("success");
 }
 
-function isValidEmail(email) {
+function checkEmail(input) {
     const re = /\S+@\S+\.\S+/;
-    return re.test(email);
+    
+    if(re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
+}
+
+function checkRequired(inputArr) {
+    inputArr.forEach(function(input) {
+        if(input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required!`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function checkInputLength(input, min) {
+    if(input.value.length < min ) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+function checkPasswordsMatch(password, password2) {
+    if(!password.value === password2.value) {
+        showError(password2, 'Passwords do not match');
+    }
 }
 
 form.addEventListener("submit", function(event){
     event.preventDefault();
-    
-    if(username.value === '') {
-        showError(username, 'Username is required');
-    } else {
-        showSuccess(username);
-    }
 
-    if(email.value === '' ) {
-        showError(email, 'email is required');
-    } else if (!isValidEmail(email.value)) {
-        showError(email, 'Email is not valid')
-    }
-     else {
-        showSuccess(email);
-    }
-
-    if(password.value === '') {
-        showError(password, 'password is required');
-    } else {
-        showSuccess(password);
-    }
-
-    if(password2.value === '') {
-        showError(password2, 'password2 is required');
-    } else {
-        showSuccess(password2);
-    }
+   checkRequired([username, email, password, password2]);
+   checkInputLength(username, 3);
+   checkInputLength(password, 6);
+   checkInputLength(password2, 6);
+   checkEmail(email);
+   checkPasswordsMatch(password, password2);
 });
-
-console.log(isValidEmail('abc.com'));
