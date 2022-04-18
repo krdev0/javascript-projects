2ac1949a80eb81901aad0c19c69011680cf1d86d
Kristaps Ritins <kritinsdev@gmail.com>
2022-04-18 22:02:37 +0300

    exchange calc base script setup

--- Exchange Rate Calculator/script.js
+++ Exchange Rate Calculator/script.js
@@ -1,2 +1,22 @@
 const API_KEY = "b5a6850a6dd8df4355aff3e5";
 
+const currencyFirst = document.querySelector("#currency-one");
+const amountFirst = document.querySelector("#amount-one");
+
+const currencySecond = document.querySelector("#currency-two");
+const amountSecond = document.querySelector("#amount-two");
+
+const rate = document.querySelector("#rate");
+const swap = document.querySelector("#swap");
+
+// Fetch rates and update DOM
+function calculate() {
+    console.log("rannn");
+}
+
+calculate();
+
+currencyFirst.addEventListener("change", calculate);
+amountFirst.addEventListener("input", calculate);
+currencySecond.addEventListener("change", calculate);
+amountSecond.addEventListener("input", calculate);
