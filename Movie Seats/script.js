20fb8599d7e11e175b5717129e8b1145c474b786
Kristaps Ritins <kritinsdev@gmail.com>
2022-04-23 12:23:30 +0300

    saving movie data to local storage

--- Movie Seats/script.js
+++ Movie Seats/script.js
@@ -4,10 +4,38 @@ const count = document.querySelector("#count");
 const total = document.querySelector("#total");
 const movieSelect = document.querySelector("#movie");
 
-const ticketPrice = Number(movieSelect.value);
+let ticketPrice = Number(movieSelect.value);
+
+function saveMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+function updateSelectedSeatCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatsIndex = [...selectedSeats]
+        .map((seat) => [...seats].indexOf(seat));
+
+    
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.textContent = selectedSeatsCount;
+    total.textContent = selectedSeatsCount * ticketPrice;
+}
+
+movieSelect.addEventListener("change", function(e) {
+    ticketPrice = Number(e.target.value);
+    saveMovieData(e.target.selectedIndex, Number(e.target.value));
+    updateSelectedSeatCount();
+});
 
 container.addEventListener("click", function(e) {
     if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
         e.target.classList.toggle("selected");
     }
+
+    updateSelectedSeatCount();
 });
